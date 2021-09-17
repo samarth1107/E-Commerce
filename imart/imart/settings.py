a975abd322a072e891a23913d88f8c93ad5fb60e
samarth1107 <samarth1107@gmail.com>
2021-09-17 15:41:00 +0530

    Templates added, url setup

--- imart/imart/settings.py
+++ imart/imart/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'buyer.apps.BuyerConfig',
+    'core.apps.CoreConfig',
+    'seller.apps.SellerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
