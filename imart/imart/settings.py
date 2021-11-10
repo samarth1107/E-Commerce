298c9ac36334d13c3781fd5ed3843a04b0ae15d0
samarth1107 <samarth1107@gmail.com>
2021-11-10 23:33:07 +0530

    Design changes

--- imart/imart/settings.py
+++ imart/imart/settings.py
@@ -125,15 +125,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT= os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'core/static'),
-)
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'image')
-MEDIA_URL = '/media/'
+if(DEBUG==True):
+    STATIC_URL = '/static/'
+    MEDIA_URL = ''
+    STATICFILES_DIRS=[os.path.join(BASE_DIR,'core/static')]
+    STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+    MEDIA_ROOT=BASE_DIR
+else:
+    STATIC_URL = '/static/'
+    MEDIA_URL=''
+    STATIC_ROOT=os.path.join(BASE_DIR,'static')
+    MEDIA_ROOT=BASE_DIR
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
